a9bb658253f0643eb5bc1cb7739089823babadea
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-08-31 02:40:44 +0300

    Adds Cli project

--- models/base.py
+++ models/base.py
@@ -1,10 +1,8 @@
-# models/base.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///train_and_gain.db"
-
-engine = create_engine(DATABASE_URL, echo=False)
+DB_URI = "sqlite:///train_and_gain.db"
+engine = create_engine(DB_URI, echo=False)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
